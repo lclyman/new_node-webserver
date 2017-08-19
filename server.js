05596220b93ae523148dbb2000476823776c52c2
lclyman <lclyman@gmail.com>
2017-08-19 11:52:08 -0600

    test again..

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next)=>{
 
 app.use(express.static(__dirname + '/public'));
 
-// views ****************************
+// views *************************
 
 app.get('/', (req, res) => {
   
